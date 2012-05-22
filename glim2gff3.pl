d39682ca2a1fb4cbd568f013c9c5e6bb0f9c84dc
minillinim <mike@mikeimelfort.com>
2012-05-22 22:07:16 +1000

    here's all the code

---
+++ glim2gff3.pl
@@ -0,0 +1,306 @@
+#!/usr/bin/env perl
+###############################################################################
+#
+#    glim2gff3.pl
+#
+#    Convert glimmer output to a gff3 file
+#
+#    Copyright (C) Michael Imelfort
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+#pragmas
+use strict;
+use warnings;
+
+#core Perl modules
+use Getopt::Long;
+use Carp;
+
+#CPAN modules
+
+#locally-written modules
+
+BEGIN {
+    select(STDERR);
+    $| = 1;
+    select(STDOUT);
+    $| = 1;
+}
+
+# edit here to log all external commands 
+my $global_log_commands = 0;
+
+# ext command failure levels
+use constant {
+    IGNORE_FAILURE => 0,
+    WARN_ON_FAILURE => 1,
+    DIE_ON_FAILURE => 2
+};
+
+# get input params and print copyright
+printAtStart();
+my $global_options = checkParams();
+
+######################################################################
+# CODE HERE
+######################################################################
+
+my $GLIM_FILE = openRead($global_options->{'in'});
+my $GFF3_FILE = openWrite($global_options->{'out'});
+print $GFF3_FILE "##gff-version 3\n";
+
+my $con_header = "";
+while(<$GLIM_FILE>)
+{
+    chomp $_;
+    if($_ =~ /^>/)
+    {
+        # header line
+        $_ =~ s/>//;
+        $con_header = $_;
+    }
+    else
+    {
+        #data line
+        $_ =~ s/(\d) /$1\t/g;
+        $_ =~ s/ //g;
+        my @fields = split /\t/, $_;
+        
+        next if(int($fields[1]) > int($fields[2]));
+        
+        my $strand = '-';
+        if($fields[3] =~ /^\+/) { $strand = '+'};
+        my $phase = abs(int($fields[3]))  - 1;
+        $phase = 0;
+        
+        print $GFF3_FILE "$con_header\t.\torf\t$fields[1]\t$fields[2]\t$fields[4]\t$strand\t$phase\tname=$fields[0]\n";
+    }
+}
+
+close $GLIM_FILE;
+close $GFF3_FILE;
+
+######################################################################
+# TEMPLATE SUBS
+
+######################################################################
+# PARAMETERS
+
+sub checkParams {
+    my @standard_options = ( "help+", "in:s", "out:s" );
+    my %options;
+
+    # Add any other command line options, and the code to handle them
+    # 
+    GetOptions( \%options, @standard_options );
+
+    # if no arguments supplied print the usage and exit
+    #
+    exec("pod2usage $0") if (0 == (keys (%options) ));
+
+    # If the -help option is set, print the usage and exit
+    #
+    exec("pod2usage $0") if $options{'help'};
+
+    # Compulsory items
+    if(!exists $options{'in'} ) { printParamError("No file to parse"); }
+    #if(!exists $options{''} ) { printParamError (""); }
+
+    return \%options;
+}
+
+sub printParamError
+{
+    #-----
+    # What to do if there's something wrong with a parameter
+    #  
+    my ($error) = @_;  
+    print "**ERROR: $0 : $error\n"; exec("pod2usage $0");
+}
+
+sub overrideDefault
+{
+    #-----
+    # Set and override default values for parameters
+    #
+    my ($default_value, $option_name) = @_;
+    if(exists $global_options->{$option_name}) 
+    {
+        return $global_options->{$option_name};
+    }
+    return $default_value;
+}
+
+######################################################################
+# FILE IO
+
+sub openWrite
+{
+    #-----
+    # Open a file for writing
+    #
+    my ($fn) = @_;
+    open my $fh, ">", $fn or croak "**ERROR: could not open file: $fn for writing $!\n";
+    return $fh;
+}
+
+sub openRead
+{   
+    #-----
+    # Open a file for reading
+    #
+    my ($fn) = @_;
+    open my $fh, "<", $fn or croak "**ERROR: could not open file: $fn for reading $!\n";
+    return $fh;
+}
+
+######################################################################
+# EXTERNAL COMMANDS
+#
+# checkAndRunCommand("ls", {
+#                          -a => ""
+#                          }, 
+#                          WARN_ON_FAILURE);
+
+sub checkFileExists {
+    #-----
+    # Does a file exists?
+    #
+    my ($file) = @_;
+    unless(-e $file) {
+        croak "**ERROR: $0 : Cannot find:\n$file\n";
+    }
+}
+
+sub logExternalCommand
+{
+    #-----
+    # Log a command line command to the command line!
+    #
+    if(1 == $global_log_commands) {
+        print $_[0], "\n";
+    }
+}
+
+sub isCommandInPath
+{
+    #-----
+    # Is this command in the path?
+    #
+    my ($cmd, $failure_type) = @_;
+    if (system("which $cmd |> /dev/null")) {
+        handleCommandFailure($cmd, $failure_type);
+    }
+}
+
+sub runExternalCommand
+{
+    #-----
+    # Run a command line command on the command line!
+    #
+    my ($cmd) = @_;
+    logExternalCommand($cmd);
+    system($cmd);
+}
+
+sub checkAndRunCommand
+{
+    #-----
+    # Run external commands more sanelier
+    # 
+    my ($cmd, $params, $failure_type) = @_;
+    
+    isCommandInPath($cmd, $failure_type);
+    
+    # join the parameters to the command
+    my $param_str = join " ", map { $_ . " " . $params->{$_}} keys %{$params};
+    my $cmd_str = $cmd . " " . $param_str;
+    
+    logExternalCommand($cmd_str);
+
+    # make sure that all went well    
+    if (system($cmd_str)) {
+         handleCommandFailure($cmd_str, $failure_type)
+    }
+}
+
+sub handleCommandFailure {
+    #-----
+    # What to do when all goes bad!
+    #
+    my ($cmd, $failure_type) = @_;
+    if (defined($failure_type)) {
+        if ($failure_type == DIE_ON_FAILURE) {
+            croak "**ERROR: $0 : " . $! . "\n";
+        } elsif ($failure_type == WARN_ON_FAILURE) {
+            carp "**WARNING: $0 : "  . $! . "\n";
+        }
+    }
+}
+
+######################################################################
+# MISC
+
+sub printAtStart {
+print<<"EOF";
+---------------------------------------------------------------- 
+ $0
+ Copyright (C) Michael Imelfort
+    
+ This program comes with ABSOLUTELY NO WARRANTY;
+ This is free software, and you are welcome to redistribute it
+ under certain conditions: See the source for more details.
+---------------------------------------------------------------- 
+EOF
+}
+
+__DATA__
+
+=head1 NAME
+
+    glim2gff3.pl
+
+=head1 COPYRIGHT
+
+   copyright (C) Michael Imelfort
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=head1 DESCRIPTION
+
+    Convert glimmer output to a gff3 file
+
+=head1 SYNOPSIS
+
+    glim2gff3.pl -in GLIM_FILE -out GFF3_FILE [-help]
+
+      -in GLIM_FILE      Output from glimmer3
+      -out GFF3_FILE     gff3 file to create
+      [-help]            Displays basic usage information
+         
+=cut
